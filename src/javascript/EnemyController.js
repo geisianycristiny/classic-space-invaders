4ccadcdde9c77406f892fe38ca1d6e446013f956
geisianycristiny <geisianycristiny7@gmail.com>
2024-05-27 10:21:12 -0400

    add collideWith  function in EnemyController class

--- src/javascript/EnemyController.js
+++ src/javascript/EnemyController.js
@@ -121,5 +121,8 @@ export default class EnemyController {
             });
         }
     )}
+    collideWith(sprite) {
+        return this.enemyRows.flat().some((enemy) => enemy.collideWith(sprite));
+    }
    
 } 
