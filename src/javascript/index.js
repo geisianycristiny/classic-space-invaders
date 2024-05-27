8c6d37250e92ef2e675b0ec20cb92524146029f9
geisianycristiny <geisianycristiny7@gmail.com>
2024-05-27 11:17:40 -0400

    add index.js and its call in index.html file

---
+++ src/javascript/index.js
@@ -0,0 +1,12 @@
+import EnemyController from "./EnemyController.js";
+import BulletController from "./BulletController.js";
+import player from "./player.js";
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
+const background = new Image();
+background.src = "./src/assets/imagens/space.png";
