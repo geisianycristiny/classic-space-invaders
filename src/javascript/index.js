e3fdda6d45933a8bde0a38ef11ac5170ff48f73e
geisianycristiny <geisianycristiny7@gmail.com>
2024-05-29 10:11:21 -0400

    add checkGame function to check game over

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -16,3 +16,27 @@ const playerBulletController = new player(canvas, 10, "white", true);
 const enemyController = new EnemyController(canvas, enemyBulletController, playerBulletController);
 const player = new player(canvas, 10, playerBulletController);
 
+let isGameOver = false;
+let didWin = false;
+
+function game() {
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+}
+
+function checkGameOver() {
+    if(isGameOver) {
+        return;
+    }
+    if(enemyBulletController.collideWith(player)) {
+        isGameOver = true;
+    }
+
+    if(enemyController.collideWith(player)) {
+        isGameOver = true;
+    }
+
+    if(enemyController.enemyRows.length === 0) {
+        didWin = true;
+        isGameOver = true;
+    }
+}
